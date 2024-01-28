6a306b70e08f7558ec279eafeefa89c869b90bbc
katoDar <katomyr@gmail.com>
2024-01-28 17:01:41 +0300

    new branch "develop"

--- config/settings.py
+++ config/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'home_work',
         'USER': 'postgres',
-        'PASSWORD': 'darling87!',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
