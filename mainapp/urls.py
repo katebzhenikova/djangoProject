62d059f1841fda28656fa4c8a1acf5c834569089
katoDar <katomyr@gmail.com>
2024-02-26 20:24:41 +0300

    home_work(blog)

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib import admin
-from mainapp.views import index, products, product_page
+from mainapp.views import ProductListView, ProductDetailView, BlogCreateView, BlogListView, BlogDetailView, \
+    BlogUpdateView, BlogDeleteView
 
 from mainapp.apps import MainappConfig
 
@@ -9,6 +10,12 @@ app_name = MainappConfig.name
 urlpatterns = [
     # path('', index, name='index'),
     # path('<int:pk>/products/', products, name='products'),
-    path('', products, name='products'),
-    path('<int:pk>/product_page/', product_page, name='product_page'),
+    path('', ProductListView.as_view(), name='products'),
+    path('<int:pk>/product_page/', ProductDetailView.as_view(), name='product_page'),
+    path('', ProductDetailView.as_view(), name='product_page'),
+    path('blog_form_create/', BlogCreateView.as_view(), name='blog_form_create'),
+    path('blog_list/', BlogListView.as_view(), name='blog_list'),
+    path('<int:pk>/blog_detail/', BlogDetailView.as_view(), name='blog_detail'),
+    path('<int:pk>/blog_form/', BlogUpdateView.as_view(), name='blog_form'),
+    path('<int:pk>/blog_confirm_delete/', BlogDeleteView.as_view(), name='blog_confirm_delete'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,36 +1,102 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
 
-from mainapp.models import Category, Product
+from mainapp.models import Category, Product, Blog
 
-
-def index(request):
-    context = {
-        'object_list': Category.objects.all(),
-        'title': 'SHOP online'
-    }
-    return render(request, 'main/index.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'main/products.html'
 
 
-def products(request):
-    context = {
-        'product_list': Product.objects.all(),
-        'title': 'SHOP online - Products'
-    }
-    return render(request, 'main/products.html', context)
+# def products(request):
+#     context = {
+#         'product_list': Product.objects.all(),
+#         'title': 'SHOP online - Products'
+#     }
+#     return render(request, 'main/products.html', context)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'main/product_page.html'
 
 
-def product_page(request, pk):
-    product_item = Product.objects.get(pk=pk)
-    context = {
-        'product_page': Product.objects.filter(product_category_id=pk),
-        'product': product_item,
-        'title': 'SHOP online - Products'
+# def product_page(request, pk):
+#     product_item = Product.objects.get(pk=pk)
+#     context = {
+#         'product_page': Product.objects.filter(product_category_id=pk),
+#         'product': product_item,
+#         'title': 'SHOP online - Products'
+#
+#     }
+#     return render(request, 'main/product_page.html', context)
 
-    }
-    return render(request, 'main/product_page.html', context)
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('blog_title', 'description', 'blog_image')
+    success_url = reverse_lazy('mainapp:blog_list')
+    template_name = 'main/blog_form.html'
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.blog_title)
+            new_blog.save()
+        return super().form_valid(form)
 
 
+class BlogListView(ListView):
+    model = Blog
+    fields = ('blog_title', 'description', 'blog_image')
+    success_url = reverse_lazy('mainapp:blog_list')
+    template_name = 'main/blog_list.html'
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(blog_is_published=True)
+        return queryset
+
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    fields = ('blog_title', 'description', 'blog_image')
+    success_url = reverse_lazy('mainapp:blog_detail')
+    template_name = 'main/blog_detail.html'
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('blog_title', 'description', 'blog_image')
+    success_url = reverse_lazy('mainapp:blog_list')
+    template_name = 'main/blog_form.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    fields = ('blog_title', 'description', 'blog_image')
+    success_url = reverse_lazy('mainapp:blog_list')
+    template_name = 'main/blog_confirm_delete.html'
+
+
+
+
+
+
+
+
+# def index(request):
+#     context = {
+#         'object_list': Category.objects.all(),
+#         'title': 'SHOP online'
+#     }
+#     return render(request, 'main/index.html', context)
 
 # def products(request, pk):
 #     if pk == 0:
