8c63cc5cccf410bd74dd492d5af8a1076a5c7b39
katoDar <katomyr@gmail.com>
2024-02-26 21:11:27 +0300

    home_work(blog) class BlogUpdateView changed

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from pytils.translit import slugify
 
@@ -74,8 +74,17 @@ class BlogDetailView(DetailView):
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('blog_title', 'description', 'blog_image')
-    success_url = reverse_lazy('mainapp:blog_list')
     template_name = 'main/blog_form.html'
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.blog_title)
+            new_blog.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('mainapp:blog_detail', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
@@ -91,23 +100,7 @@ class BlogDeleteView(DeleteView):
 
 
 
-# def index(request):
-#     context = {
-#         'object_list': Category.objects.all(),
-#         'title': 'SHOP online'
-#     }
-#     return render(request, 'main/index.html', context)
 
-# def products(request, pk):
-#     if pk == 0:
-#         product_item = Category.objects.all()
-#     else:
-#         product_item = Category.objects.get(pk=pk)
-#     context = {
-#         'object_list': Product.objects.filter(product_category_id=pk),
-#         'products': product_item
-#     }
-#     return render(request, 'main/products.html', context)
 
 
 
